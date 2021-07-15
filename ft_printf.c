def1e63924a475bc7773dbb164555a1398381793
Jorge Vacarisas bofill <jvacaris@c1r15s2.42madrid.com>
2021-07-15 23:04:30 +0200

    Starting p. string and char works :D

--- ft_printf.c
+++ ft_printf.c
@@ -110,10 +110,23 @@ int	ft_printf(const char *masstr, ...)
 
 int main()
 {
-	write(1, "Yours: ", 7);
-	int a = ft_printf("Hello World %s Hello World", "Goodbye");
+	int z;
+
+	z = 0;
+//	write(1, "\nYours: ", 8);
+//	int a = ft_printf("Hello World %s Hello World", "Goodbye");
 	write(1, "\nOrgnl: ", 8);
-	int b = printf("Hello World %s Hello World", "Goodbye");
-	printf("\n\nft_printf returned: %d", a);
+	int b = printf("Hello World %p Hello World", &z);
+//	printf("\n\nft_printf returned: %d", a);
 	printf("\n   printf returned: %d", b);
+/*	if (a == b)
+		printf("\n\nBoth printf's RETURNED the same value (check printed values)\n\n");
+	else
+		printf("\n\nSomething went wrong :(\n\n");*/
+
+	char *f;
+	f = malloc(sizeof(char) * 15);
+	f[0] = &z[0]; 
+	printf(">>%s<<", f);
+	free (f);
 }
--- ft_s.c
+++ ft_s.c
@@ -9,36 +9,50 @@ t_brain	spaceandcount(t_brain brain)
 	brain.totalen++;
 	return (brain);
 }
-/************************************
- *                                  *
- *  This has to be changed so the   *
- *  string shortening is the first  *
- *   thing to happen. Make a new    *
- *   variable (string2) to place    *
- *    the new shortened string.     *
- *                                  *
- *                 ||               *
- *                 ||               *
- *              \\ || //            *
- *               \\||//             *
- *                \  /              *
- *                 \/                *
- ************************************/
+
+char	*stringtrimmer(t_brain brain, char *inistring)
+{
+	char	*newstring;
+	int		a;
+
+	a = 0;
+	if ((ft_strlen(inistring) > brain.imaxchar && brain.imaxchar) || brain.imaxcharzero)
+	{
+		newstring = malloc (brain.imaxchar + 1);
+		while (a < brain.imaxchar)
+		{
+			newstring[a] = inistring[a];
+			a++;
+		}
+		newstring[a] = 0;
+		return (newstring);
+	}
+	else
+		return (inistring);
+}
 
 t_brain	ft_s(t_brain brain)
 {
 	int		dif;
 	char	*string = NULL;
-	printf(">>%d<<", brain.totalen);
+//	printf(">>%d<<", brain.totalen);
 //	write(1, "It's a string!", 14);
-	string = va_arg (*brain.args, char *);
+	string = stringtrimmer(brain, va_arg (*brain.args, char *));
 	if (ft_strlen(string) < brain.iminchar && !brain.bneg)
 	{
 		dif = brain.iminchar;
 		while (dif-- - ft_strlen(string))
 			brain = spaceandcount(brain);
 	}
-	if ((ft_strlen(string) > brain.imaxchar && brain.imaxchar) || brain.imaxcharzero)
+
+	dif = 0;
+	while (string[dif])
+	{
+		write(1, &string[dif], 1);
+		brain.totalen++;
+		dif++;
+	}
+/*	if ((ft_strlen(string) > brain.imaxchar && brain.imaxchar) || brain.imaxcharzero)
 	{
 		dif = 0;
 		if (!brain.bneg && brain.imaxchar < brain.iminchar)
@@ -55,14 +69,14 @@ t_brain	ft_s(t_brain brain)
 		if (brain.bneg && brain.imaxchar < brain.iminchar)
 			while (ft_strlen(string) - dif++ > brain.imaxchar)
 				spaceandcount(brain);
-	}
+	}*/
 	if (ft_strlen(string) < brain.iminchar && brain.bneg)
 	{
 		dif = brain.iminchar;
 		while (dif-- - ft_strlen(string))
 			brain = spaceandcount(brain);
 	}
-	dif = 0;
+/*	dif = 0;
 	if (ft_strlen(string) < brain.imaxchar && ft_strlen(string) > brain.iminchar)
 	{
 		while (string[dif])
@@ -71,7 +85,7 @@ t_brain	ft_s(t_brain brain)
 			brain.totalen++;
 			dif++;
 		}
-	}
-	printf(">>.%d.<<", brain.totalen);
+	}*/
+//	printf(">>.%d.<<", brain.totalen);
 	return (brain);
 }
