4cc56794614fb395920516ab8c8584f2dad0294e
Jorge Vacarisas bofill <jvacaris@c1r15s5.42madrid.com>
2021-07-20 18:00:29 +0200

    Previous commit Moulinette failed. Possible SegFault with old Mouli fixed

--- ft_paramdetector.c
+++ ft_paramdetector.c
@@ -34,31 +34,40 @@ static t_brain	typesorter(t_brain brain)
 	return (brain);
 }
 
+static t_brain	paramdetectortwo(t_brain brain)
+{
+	if (ft_isalpha(brain.strin[brain.loc]) || brain.strin[brain.loc] == '%')
+		brain.ctype = brain.strin[brain.loc];
+	else if (brain.strin[brain.loc] == '-')
+		brain.bneg = 1;
+	else if (brain.strin[brain.loc] == '0' && !brain.bdot && !brain.imnchr)
+		brain.bzero = 1;
+	else if (brain.strin[brain.loc] == '.')
+		brain.bdot = 1;
+	else if (brain.strin[brain.loc] == '+')
+		brain.bplus = 1;
+	else if (brain.strin[brain.loc] == ' ')
+		brain.bspace = 1;
+	else if (brain.strin[brain.loc] == '#')
+		brain.bhash = 1;
+	else if (ft_isdigit(brain.strin[brain.loc]) && !brain.bdot)
+		brain.imnchr = brain.imnchr * 10 + (brain.strin[brain.loc] - 48);
+	else if (ft_isdigit(brain.strin[brain.loc]) && brain.bdot)
+		brain.imxchr = brain.imxchr * 10 + (brain.strin[brain.loc] - 48);
+	else
+		brain.bemergencybreak = 1;
+	brain.loc++;
+	return (brain);
+}
+
 t_brain	paramdetector(t_brain brain)
 {
 	brain.loc++;
-	while (brain.ctype == '-')
+	while (brain.ctype == '-' && !brain.bemergencybreak)
 	{
-		if (ft_isalpha(brain.strin[brain.loc]) || brain.strin[brain.loc] == '%')
-			brain.ctype = brain.strin[brain.loc];
-		else if (brain.strin[brain.loc] == '-')
-			brain.bneg = 1;
-		else if (brain.strin[brain.loc] == '0' && !brain.bdot && !brain.imnchr)
-			brain.bzero = 1;
-		else if (brain.strin[brain.loc] == '.')
-			brain.bdot = 1;
-		else if (brain.strin[brain.loc] == '+')
-			brain.bplus = 1;
-		else if (brain.strin[brain.loc] == ' ')
-			brain.bspace = 1;
-		else if (brain.strin[brain.loc] == '#')
-			brain.bhash = 1;
-		else if (ft_isdigit(brain.strin[brain.loc]) && !brain.bdot)
-			brain.imnchr = brain.imnchr * 10 + (brain.strin[brain.loc] - 48);
-		else if (ft_isdigit(brain.strin[brain.loc]) && brain.bdot)
-			brain.imxchr = brain.imxchr * 10 + (brain.strin[brain.loc] - 48);
-		brain.loc++;
+		brain = paramdetectortwo(brain);
 	}
-	brain = typesorter(brain);
+	if (!brain.bemergencybreak)
+		brain = typesorter(brain);
 	return (brain);
 }
